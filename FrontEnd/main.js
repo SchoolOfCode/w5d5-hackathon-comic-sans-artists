32a26f6a5339e8c7f50dc36836d59e5bba578c51
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-05 17:59:08 +0000

    styling and started js

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,60 +1,17 @@
-const submitButton = document.querySelector("#submit-button");
+const buddyList = document.querySelector(".birthday-buddies");
+const monthBtns = document.querySelectorAll(".month");
+const submitBtn = document.querySelector("#submit-button");
 
-const BACKEND_URL = "http://localhost:5000";
+const backendURL = "http://localhost:5000";
 
-async function loadUsers(month) {
-    const res = await fetch(`${BACKEND_URL}/users/april`);
+async function getData(query) {
+    const res = await fetch(`${backendURL}/users/${query}`);
     const data = await res.json();
 
-    console.log(data);
-    data.forEach(user => renderUser(user));
+    console.log(data[0])
 }
 
-async function sendUser(path, method, body = '') {
-	const res = await fetch(`${BACKEND_URL}${path}`, {
-		method,
-		headers : {
-			'content-type' : 'application/json',
-		},
-		body    : JSON.stringify(body),
-	});
-	const data = await res.json();
-	return data;
-}
+getData("");
 
-async function addNewUser() {
-    event.preventDefault();
-    window.location.href="month-compare.html"
+monthBtns.forEach(button => button.addEventListener("click", () => console.log(button.innerText)));
 
-    const user = {
-		name    : event.target[0].value,
-		day : event.target[1].value,
-		month : event.target[2].value,
-    };
-    console.log(user);
-    
-    await sendUser('/users', 'POST', user);
-    event.target.reset();
-}
-
-submitButton.addEventListener("submit", addNewUser);
-
-const monthButtons = document.querySelectorAll(".month")
-
-function renderUser(user) {
-	const { id, name, day, monthBorn } = user;
-
-	const p = document.createElement('p');
-	p.innerHTML = `${name}: {day}th {monthBorn}.`;
-
-	body.appendChild(user);
-}
-
-
-async function getByMonth(month) {
-    window.event.preventDefault()
-
-    loadUsers(month);
-}
-
-monthButtons.forEach(button => button.addEventListener("click", () => getByMonth(button.InnerText)));
