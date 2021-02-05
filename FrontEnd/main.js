49e4eeebb095d69a856ddacf8c5bafaddc3b1c91
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-05 16:00:29 +0000

    JS attempt

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -10,34 +10,34 @@ async function loadUsers(month) {
     data.forEach(user => renderUser(user));
 }
 
-// async function sendUser(path, method, body = '') {
-// 	const res = await fetch(`${BACKEND_URL}${path}`, {
-// 		method,
-// 		headers : {
-// 			'content-type' : 'application/json',
-// 		},
-// 		body    : JSON.stringify(body),
-// 	});
-// 	const data = await res.json();
-// 	return data;
-// }
+async function sendUser(path, method, body = '') {
+	const res = await fetch(`${BACKEND_URL}${path}`, {
+		method,
+		headers : {
+			'content-type' : 'application/json',
+		},
+		body    : JSON.stringify(body),
+	});
+	const data = await res.json();
+	return data;
+}
 
-// async function addNewUser() {
-//     event.preventDefault();
-//     window.location.href="month-compare.html"
+async function addNewUser() {
+    event.preventDefault();
+    window.location.href="month-compare.html"
 
-//     const user = {
-// 		name    : event.target[0].value,
-// 		day : event.target[1].value,
-// 		month : event.target[2].value,
-//     };
-//     console.log(user);
+    const user = {
+		name    : event.target[0].value,
+		day : event.target[1].value,
+		month : event.target[2].value,
+    };
+    console.log(user);
     
-//     await sendUser('/users', 'POST', user);
-//     event.target.reset();
-// }
+    await sendUser('/users', 'POST', user);
+    event.target.reset();
+}
 
-// submitButton.addEventListener("submit", addNewUser);
+submitButton.addEventListener("submit", addNewUser);
 
 const monthButtons = document.querySelectorAll(".month")
 
