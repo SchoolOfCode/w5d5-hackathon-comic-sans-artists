25fbebecabd92fa6abafd2afb9a950aa76efd2a0
Stella Taylor <stellataylor10@gmail.com>
2021-02-05 15:15:22 +0000

    started js edited font

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,3 +1,10 @@
 const submitButton = document.querySelector("#submit-button");
 
-const BACKEND_URL = 
+const BACKEND_URL = "http://localhost:5001";
+
+async function loadInitialUsers() {
+    const res = await fetch(`${BACKEND_URL}/users`);
+    const data = await res.json();
+    data.forEach(renderUser);
+}
+
