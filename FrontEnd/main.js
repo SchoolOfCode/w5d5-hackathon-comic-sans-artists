7a168755746b5c8f2e9741eae6a1379eee6fbb8a
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-05 15:48:04 +0000

    JS attempt

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,10 +1,60 @@
 const submitButton = document.querySelector("#submit-button");
 
-const BACKEND_URL = "http://localhost:5001";
+const BACKEND_URL = "http://localhost:5000";
 
-async function loadInitialUsers() {
-    const res = await fetch(`${BACKEND_URL}/users`);
+async function loadUsers(month) {
+    const res = await fetch(`${BACKEND_URL}/users/april`);
     const data = await res.json();
-    data.forEach(renderUser);
+
+    console.log(data);
+    data.forEach(user => renderUser(user));
 }
 
+// async function sendUser(path, method, body = '') {
+// 	const res = await fetch(`${BACKEND_URL}${path}`, {
+// 		method,
+// 		headers : {
+// 			'content-type' : 'application/json',
+// 		},
+// 		body    : JSON.stringify(body),
+// 	});
+// 	const data = await res.json();
+// 	return data;
+// }
+
+// async function addNewUser() {
+//     event.preventDefault();
+//     window.location.href="month-compare.html"
+
+//     const user = {
+// 		name    : event.target[0].value,
+// 		day : event.target[1].value,
+// 		month : event.target[2].value,
+//     };
+//     console.log(user);
+    
+//     await sendUser('/users', 'POST', user);
+//     event.target.reset();
+// }
+
+// submitButton.addEventListener("submit", addNewUser);
+
+const monthButtons = document.querySelectorAll(".month")
+
+function renderUser(user) {
+	const { id, name, day, monthBorn } = user;
+
+	const p = document.createElement('p');
+	p.innerHTML = `${name}: {day}th {monthBorn}.`;
+
+	body.appendChild(user);
+}
+
+
+async function getByMonth(month) {
+    window.event.preventDefault()
+
+    loadUsers(month);
+}
+
+monthButtons.forEach(button => button.addEventListener("click", () => getByMonth(button.InnerText)));
