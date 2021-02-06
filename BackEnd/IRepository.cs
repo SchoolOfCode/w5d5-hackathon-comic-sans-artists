b1ab1517ddf04578644c0c2ab66ea5adb38a8bf8
Nathan Hinks <nathanhinks.dev@gmail.com>
2021-02-06 18:58:03 +0000

    comments added

--- BackEnd/IRepository.cs
+++ BackEnd/IRepository.cs
@@ -2,8 +2,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 public interface IRepository<T>
 {
-    Task<IEnumerable<T>> GetAll();
-    // Task<IEnumerable<T>> GetByName(); - maybe later
-    Task<IEnumerable<T>> GetAllByMonth(string month);
-    Task<T> Insert(T item);
+    Task<IEnumerable<T>> GetAll(); //called on page load
+    Task<IEnumerable<T>> GetAllByMonth(string month); //on button clicks
+    Task<T> Insert(T item); //from the form
 }
